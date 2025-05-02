5ab841221218d591a885fa69185bb914eec75921
137 <113233555+caezium@users.noreply.github.com>
2025-05-03 03:58:10 +0800

    Change Localizer measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Localizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Localizer.java
@@ -10,14 +10,13 @@ import org.opencv.core.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class Localizer{
+public class Localizer {
     public static DcMotor podL, podR, podM;
     HardwareMap hardwareMap;
-    public static double C = (35 * 3.14)/8192; // (r*pi)/N(encoder counts for 1 loop)
-    public static double Ly = 129.5; // distance between L wheels and y
-    public static double Ry = -138.4; // distance between R wheels and y
-    public static double Bx = 44.5; // distance between center wheel and x
+    public static double C = (35 * 3.14) / 8192; // (r*pi)/N(encoder counts for 1 loop)
+    public static double Ly = 42; // distance between L wheels and y mm
+    public static double Ry = -34; // distance between R wheels and y
+    public static double Bx = 4.12; // distance between center wheel and x
     // initial values
     public static double X = 0;
     public static double Y = 0;
@@ -27,7 +26,8 @@ public class Localizer{
     public static double dL, dR, dM;
 
     IMU imu;
-    public void initialize(HardwareMap map, String podLname, String podRname, String podMname){
+
+    public void initialize(HardwareMap map, String podLname, String podRname, String podMname) {
         hardwareMap = map;
         List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : allHubs) {
@@ -40,19 +40,21 @@ public class Localizer{
 
         imu = hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
         imu.initialize(new IMU.Parameters(orientationOnRobot));
         imu.resetYaw();
     }
 
-    public void initialize(HardwareMap map){
+    public void initialize(HardwareMap map) {
         initialize(map, expansion.motor(1), expansion.motor(0), expansion.motor(2));
     }
 
-    public void resetIMU(){ imu.resetYaw(); }
+    public void resetIMU() {
+        imu.resetYaw();
+    }
 
-    public void resetEncoder(){
+    public void resetEncoder() {
         X = 0;
         Y = 0;
         theta = 0;
@@ -64,43 +66,46 @@ public class Localizer{
         podR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         podM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-    public double heading(){ return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS); }
 
-    public static void positionArc(){
+    public double heading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    public static void positionArc() {
         double dX, dY;
         L = -podL.getCurrentPosition();
         R = -podR.getCurrentPosition();
         M = podM.getCurrentPosition();
         // encoder deltas
-        dL = (L-pL)*C;
-        dR = (R-pR)*C;
-        dM = (M-pM)*C;
+        dL = (L - pL) * C;
+        dR = (R - pR) * C;
+        dM = (M - pM) * C;
         // update previous readings
         pL = L;
         pR = R;
         pM = M;
         // Calculate
-        double fwd = (dR*Ly-dL*Ry)/(Ly-Ry); // forward
-        double dA = (dR-dL)/(Ly-Ry); // angular
-        double str = dM - Bx*dA; // strafe
+        double fwd = (dR * Ly - dL * Ry) / (Ly - Ry); // forward
+        double dA = (dR - dL) / (Ly - Ry); // angular
+        double str = dM - Bx * dA; // strafe
 
-        if(dA!=0) { // arc based calculation
+        if (dA != 0) { // arc based calculation
             double r0 = fwd / dA;
             double r1 = str / dA;
-            double rX = r0*Math.sin(dA) - r1*(1 - Math.cos(dA));
-            double rY = r1*Math.sin(dA) + r0*(1 - Math.cos(dA));
+            double rX = r0 * Math.sin(dA) - r1 * (1 - Math.cos(dA));
+            double rY = r1 * Math.sin(dA) + r0 * (1 - Math.cos(dA));
 
-            dX = rX*Math.cos(theta) - rY*Math.sin(theta);
-            dY = rX*Math.sin(theta) + rY*Math.cos(theta);
-        }else{ // straight
-            dX = fwd*Math.cos(theta) - str*Math.sin(theta);
-            dY = fwd*Math.sin(theta) + str*Math.cos(theta);
+            dX = rX * Math.cos(theta) - rY * Math.sin(theta);
+            dY = rX * Math.sin(theta) + rY * Math.cos(theta);
+        } else { // straight
+            dX = fwd * Math.cos(theta) - str * Math.sin(theta);
+            dY = fwd * Math.sin(theta) + str * Math.cos(theta);
         }
         // update current position
         X += dX;
         Y += dY;
         theta += dA;
-        //theta = angleWrap(theta);
+        // theta = angleWrap(theta);
     }
 
     public static double angleWrap(double radians) {
@@ -117,9 +122,9 @@ public class Localizer{
     public static ArrayList<Point> intersect(Point center, double r, Point line1, Point line2) {
         // find intersection of circle center-r to line line1-line2
         ArrayList<Point> all = new ArrayList<>();
-        if(line2.x-line1.x == 0){
-            double y1 = Math.sqrt(Math.pow(r,2)-Math.pow(line1.x - center.x, 2));
-            double y2 = -Math.sqrt(Math.pow(r,2)-Math.pow(line1.x - center.x, 2));
+        if (line2.x - line1.x == 0) {
+            double y1 = Math.sqrt(Math.pow(r, 2) - Math.pow(line1.x - center.x, 2));
+            double y2 = -Math.sqrt(Math.pow(r, 2) - Math.pow(line1.x - center.x, 2));
             y1 += center.y;
             y2 += center.y;
             all.add(new Point(line1.x, y1));
@@ -127,22 +132,20 @@ public class Localizer{
             return all;
         }
 
-        double m1 = (line2.y - line1.y)/(line2.x - line1.x);
-        double quadA = 1 + Math.pow(m1,2);
+        double m1 = (line2.y - line1.y) / (line2.x - line1.x);
+        double quadA = 1 + Math.pow(m1, 2);
 
         double x1 = line1.x - center.x;
         double y1 = line1.y - center.y;
 
-        double quadB = (2 * m1 * y1) - (2 * Math.pow(m1,2) * x1);
+        double quadB = (2 * m1 * y1) - (2 * Math.pow(m1, 2) * x1);
 
-        double quadC = (Math.pow(m1,2) * Math.pow(x1, 2) - (2 * y1 * m1 * x1) + Math.pow(y1,2) - Math.pow(r, 2));
+        double quadC = (Math.pow(m1, 2) * Math.pow(x1, 2) - (2 * y1 * m1 * x1) + Math.pow(y1, 2) - Math.pow(r, 2));
 
-
-
-        try{
-            double delta = Math.sqrt(Math.pow(quadB, 2) - 4 * quadA *quadC);
-            double xRoot1 = (-quadB + delta)/(2 * quadA);
-            double xRoot2 = (-quadB - delta)/(2 * quadA);
+        try {
+            double delta = Math.sqrt(Math.pow(quadB, 2) - 4 * quadA * quadC);
+            double xRoot1 = (-quadB + delta) / (2 * quadA);
+            double xRoot2 = (-quadB - delta) / (2 * quadA);
             double yRoot1 = m1 * (xRoot1 - x1) + y1;
             double yRoot2 = m1 * (xRoot2 - x1) + y1;
 
@@ -154,10 +157,14 @@ public class Localizer{
             double minX = Math.min(line1.x, line2.x);
             double maxX = Math.max(line1.x, line2.x);
 
-            if(xRoot1 > minX && xRoot1 < maxX){ all.add(new Point(xRoot1, yRoot1)); }
-            if(xRoot2 > minX && xRoot2 < maxX){ all.add(new Point(xRoot2, yRoot2)); }
+            if (xRoot1 > minX && xRoot1 < maxX) {
+                all.add(new Point(xRoot1, yRoot1));
+            }
+            if (xRoot2 > minX && xRoot2 < maxX) {
+                all.add(new Point(xRoot2, yRoot2));
+            }
 
-        }catch(Exception e){
+        } catch (Exception e) {
 
         }
         return all;
