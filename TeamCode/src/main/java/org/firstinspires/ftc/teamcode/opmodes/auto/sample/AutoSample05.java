19604c2c8de2308a5f8e94326469ce8c298a21dc
207053 <chenhongyijackson@qq.com>
2025-06-30 22:29:29 +0800

    pose & slide encoder vals transfer auto-> swerve using PoseStorage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sample/AutoSample05.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sample/AutoSample05.java
@@ -295,5 +295,7 @@ public final class AutoSample05 extends LinearOpMode {
 
                 // Save final pose for teleop
                 PoseStorage.currentPose = drive.localizer.getPose();
+                PoseStorage.LowerSlideTick = (int)lowSlide.getCurrentPosition();
+                PoseStorage.UpperSlideTick = (int)upSlide.getCurrentPosition();
         }
 }
